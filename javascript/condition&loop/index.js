64e2fc8a401157e3a1ed83e9a766210035dacdae
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-24 12:46:24 +0400

    js loops condition statements functions added

---
+++ javascript/condition&loop/index.js
@@ -0,0 +1,38 @@
+// let age = 12;
+
+// if(age >18){
+//   console.log("Xos geldiniz");
+// }else if(age<18){
+//   console.log("Xos getdiniz");
+// }else {
+//   console.log("Gozleyin!");
+// }
+
+// let command = "start";
+
+// switch(command){
+//   case "start":
+//     console.log("Basladiq");
+//   case "stop":
+//     console.log("Dayandiq");
+//   default:
+//     console.log("Hecne elemirik");
+//     break;
+// }
+
+// for(let i = 0; i< 100; i++){
+//   console.log(i);
+// }
+
+
+// let count = 12;
+
+// while (count>0) {
+//   console.log(count--);
+// }
+
+// let count = 0;
+
+// do{
+//   console.log(count++);
+// }while(count>0);
---
+++ javascript/functions/index.js
@@ -0,0 +1,19 @@
+// public int Sum(int a, int b) {
+//   return a + b;
+// }
+
+// function sum(a,b){
+//   return a+b;
+// }
+
+// function print(name){
+//   console.log("Hello ", name);
+// }
+// print("suleyman", 12, 129 ,true);
+
+function sum(a,b){
+  // return a+b;
+}
+
+let res = sum(3,"8");
+console.log(res);
