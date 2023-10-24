a61a919107a1d9f55107d14cb86d2b3985745e4c
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-24 12:14:14 +0400

    js basics added

---
+++ javascript/basics/index.js
@@ -0,0 +1,173 @@
+// console.log("Hello P140");
+// comment line
+
+/*
+  multi  
+  line
+  comment 
+*/
+
+
+//#region Variable Declaration & Data Types
+
+// int a = 12;
+
+// let a = 12;
+// let b = true;
+// let c = "hello";
+// let d = 12.2;
+// let e = 'e';
+
+// a=true;
+// a= "asd";
+
+// console.log(a);
+
+// let my_name = "suleuyman";
+// let myName = "suleyman";
+// let $myName = "suleyman";
+// let myName1 = "suleyman";
+
+// let my-name = "suleyman"
+// let 1myName = "suleyuman"
+// let #myName = "suleyuman";
+// let let ="suleyman";
+// let for ="suleyman";
+
+
+// let a = 12;
+// a = 17;
+
+// console.log(a);
+
+// number = > 16byte
+// let b = 12;
+// b = 29918273871238;
+
+// console.log("Hello");
+
+// // 4bit
+// const a = 12;
+// a= 17;
+// console.log(a);
+
+// console.log("bitti");
+
+
+// int[] numbers = new int[3];
+// int[] numbers = {2,3,4};
+
+// let numbers = [1,2,3,4];
+// numbers[0] = 44;
+// numbers[8] = 12;
+// console.log(numbers);
+// console.log("xeta yoxdur ki program bitmir");
+
+
+// let items = [1, 2, "hello", true, [1,2,[123,8,9],3] ]
+// console.log(items);
+
+// let names = ["nurane","xumar","hesonov","reyhan"]
+
+// for(let i = 0; i< names.length; i++){
+//   console.log(names[i]);
+// }
+
+// console.log("10" - "1");
+
+// let a = 1000n;
+// console.log(a);
+// let d = "hello"
+// let a = "suleyman";
+// let b = 'suleyman';
+// let c = `suleyman ${d}`
+// console.log(c);
+
+
+// let name = "suleuman"
+// let age = 12;
+// let isNew = true;
+
+// console.log(`name:${name}, age: ${age}, is new: ${isNew}`);
+// let a = null;
+// console.log(typeof a);
+
+//#endregion
+
+
+//#region Type Conversions
+
+
+//String conversion
+// let a = String(12);
+// let b = 12;
+// let a = b+"";
+
+// console.log(typeof a);
+
+// Number conversion
+// let b = "12";
+// // let a = Number(b);
+// let a = +b;
+
+// console.log(a);
+// console.log(typeof a);
+
+
+// console.log(Number("true"));
+
+
+// Boolean conversion
+// console.log(Boolean([]));
+// console.log(!!0);
+
+
+//#endregion
+
+//#region  Operations
+  // console.log(2-3);
+  // console.log(2+3);
+  // console.log(2*3);
+  // console.log(4/3);
+  // console.log(5%3);
+//#endregion
+
+//#region  String Concatination && Increment - Decrement
+  // console.log("1"+"asd");
+
+  // console.log(1 + "" + 2);
+  // console.log(1+2+""+2);
+  // console.log(1+2+""+1+2);
+
+  // let a = 12; // 13
+  // let b = 17; // 18
+
+  // console.log(a++); // 12
+  // console.log(++b); // 18
+
+//#endregion
+
+// let age = 21;
+
+// console.log(age != 21);
+
+
+
+// console.log(21 === "21");
+
+// console.log(21 !== "21");
+
+// console.log(2 == 2 || 3*100123 <= 5*12823);
+
+
+// console.log(0 || "" );
+
+// let fullname = ""
+// let name = ""
+
+// console.log(fullname || name || "yoxdur");
+
+// console.log(12 == 12 && 3 == "3");
+
+// console.log("salam" && "hello" && 0 && false);
+
