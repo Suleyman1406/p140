26196958d422d0f7cf42a06403d0b99f0e86c59e
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-25 10:43:34 +0400

    js functions added

--- javascript/functions/index.js
+++ javascript/functions/index.js
@@ -11,9 +11,50 @@
 // }
 // print("suleyman", 12, 129 ,true);
 
-function sum(a,b){
-  // return a+b;
-}
+// function sum(a,b){
+//   // return a+b;
+// }
 
-let res = sum(3,"8");
-console.log(res);
+// let res = sum(3,"8");
+// console.log(res);
+
+
+// function printHelloToSomeone(name = "Reyhan"){
+//   console.log("hello "+name);
+// }
+
+// printHelloToSomeone("Leman")
+
+// printDeclaration();
+
+// function printDeclaration (){
+//   console.log("print declaration");
+// }
+
+// print("Rustem")
+
+
+// let print = function(name){
+//   console.log("hello",name);
+// }
+
+// let printArrow = (name) => {
+//   console.log("hello",name);
+// }
+
+// printArrow("Zerbeli");
+
+// IIFE
+
+// (function(name){
+//   console.log("hello",name);
+// })("Aysel")
+
+
+// function printResult(a , b, doOperation){
+//   let res = doOperation(a,b)
+//   console.log(res);
+//   // console.log(doOperation(a,b));
+// }
+
+// printResult([1,2,3], 5, (t,y)=>{ return t[0]*y; })
