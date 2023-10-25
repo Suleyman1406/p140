1ce9f69014a33251006525d3e0b524cf3d8fae76
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-25 11:01:58 +0400

    js numbers added

---
+++ javascript/numbers/index.js
@@ -0,0 +1,20 @@
+// let cox = 100_000_000_000;
+// console.log(cox);
+// let num = 1e19;
+// console.log(num);
+
+// console.log(Number.isNaN("asd"-"asd"));
+// console.log(Number.isFinite(12));
+
+
+// console.log(parseFloat("100.5px"));
+
+// console.log(Math.random());
+
+// console.log(Math.max(2,7,-2,88,22,99,12));
+// console.log(Math.min(2,7,-2,88,22,99,12));
+
+// console.log(Math.ceil(12.00000000007));
+// console.log(Math.floor(12.9999999999));
+// console.log(Math.round(12.49999));
+
