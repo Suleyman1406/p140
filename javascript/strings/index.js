558988c0e0a5d506c479fa261e3111e1afb7c5e6
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-25 11:49:30 +0400

    js strings added

---
+++ javascript/strings/index.js
@@ -0,0 +1,44 @@
+// let myText='I\'m Suleyman'
+// console.log(myText);
+
+
+// let myText = "Hello";
+
+
+// console.log(myText.at(-5));
+
+// let myText = "Hello"
+// console.log(myText.toUpperCase());
+
+// let myText = "Hello my brother";
+
+// console.log(myText.indexOf("o",5));
+// console.log(myText.indexOf("l"));
+// console.log(myText.lastIndexOf("l"));
+
+
+// let myText = "   Hello my brother  ";
+
+// console.log(myText.includes("my "));
+// console.log(myText.startsWith("my"));
+// console.log(myText.endsWith("brothe"));
+// console.log(myText.replace("l","a"));
+// console.log(myText.replaceAll(" ","-"));
+// console.log(myText.replaceAll(" ",""));
+// console.log(myText);
+
+// console.log(myText.trim());
+// console.log(myText.trimStart());
+// console.log(myText.trimEnd());
+// console.log(myText);
+
+
+// let myText = "Hello it is me";
+
+// console.log(myText.replaceAll(" ","").split(""));
+
+// let str = "this is paragraph";
+
+// console.log(str.slice(-4,-1));
+// console.log(str.substring(0,4));
+// console.log(str.substr(1,10));
