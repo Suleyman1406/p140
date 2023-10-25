04572bfe232567c518b9efd30e72b7961a30b0e9
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-25 12:38:55 +0400

    js arrays added

---
+++ javascript/arrays/index.js
@@ -0,0 +1,72 @@
+// let myArr = [
+//   1,
+//   "str",
+//   true,
+//   [1],
+//   function () {
+//     console.log("Hello");
+//   },
+// ];
+
+// myArr[4]();
+
+// let numbers = [1, 2, 3, 5];
+// // console.log(numbers.at(-2));
+// console.log("evvel", numbers); // 1
+// // numbers.push(12, 77, 99);
+// // numbers.unshift(88, 5, 0);
+
+// numbers.pop();
+// let res = numbers.pop(); // 3
+// numbers.pop(); //
+// console.log("silinen", res); // 3
+// console.log("sonra", numbers); //1
+
+// let numbers = [1, 2, 3, 5];
+
+// numbers.shift();
+// numbers.shift();
+// let res = numbers.shift();
+// console.log(res);
+
+// console.log(numbers);
+
+// [1,2,3,4]
+
+// function change(b) {
+//   b = 18;
+// }
+
+// let a = 12;
+
+// change(a);
+
+// console.log(a);
+
+// function change(arr) {
+//   arr[0] = 4;
+// }
+
+// let myArr = [1, 2, 3];
+
+// change(myArr);
+
+// console.log(myArr);
+
+// let fruits = ["alma", "armud", "heyva"];
+
+// for (let fruit of fruits) {
+//   console.log(fruit);
+// }
+
+// let fruits = ["alma", "heyva"];
+// console.log(fruits.length);
+
+// fruits.length = 12;
+// console.log(fruits);
+
+// let myArr = new Array(10).fill("hello");
+
+// console.log(myArr);
+
+// console.log(String([1, 2, 3]));
