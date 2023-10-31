96e8afd14c508f64f859a53be21ca8e5d2eb7f9c
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-31 10:16:19 +0400

    js dom selectors added

---
+++ javascript-dom/getElement-querySelector/script.js
@@ -0,0 +1,19 @@
+// const textDiv = document.getElementById("text");
+// // console.log(textDiv);
+// textDiv.textContent = "Hello Everyone";
+
+// const asdsd = document.getElementsByTagName("div");
+
+// console.log(asdsd);
+
+// const cards = document.getElementsByClassName("card");
+// console.log(cards);
+
+// const textDiv = document.querySelector("#text");
+// console.log(textDiv.textContent);
+
+// const cards = document.querySelectorAll(".card");
+// console.log(cards);
+
+// const liItems = document.querySelectorAll("body > ul > a:first-child");
+// console.log(liItems);
