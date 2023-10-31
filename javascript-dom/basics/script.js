58a903611345315ea0c13fde272c7345715cdce7
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-31 10:16:10 +0400

    js dom basics added

---
+++ javascript-dom/basics/script.js
@@ -0,0 +1,29 @@
+// document.body.style.backgroundColor = "red"
+
+// console.log(document.body.childNodes);
+// document.body.children[0].style.color = "yellow"
+// document.body.children[0].textContent = "hello P140"
+
+// console.log();
+
+// let ulItems = Array.from(document.body.children[2].children);
+
+// ulItems.forEach((item) => console.log(item.textContent));
+
+// const list = document.body.children[2];
+
+// console.log(list.firstChild);
+// console.log(list.firstElementChild.textContent);
+// // console.log(list.lastChild);
+// console.log(list.lastElementChild.textContent);
+// console.log(list.children[0]);
+
+// console.log(
+//   list.firstElementChild.lastElementChild.parentElement.firstElementChild
+//     .lastElementChild.textContent
+// );
+// console.log(
+//   list.firstElementChild.firstElementChild.firstElementChild.nextElementSibling
+//     .previousElementSibling.nextElementSibling.nextElementSibling
+//     .nextElementSibling
+// );
