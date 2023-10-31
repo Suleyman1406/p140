bfc4e896f20f7278a2734ee3a9531bbd20e816b5
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-31 12:12:24 +0400

    js dom event added

---
+++ javascript-dom/event/script.js
@@ -0,0 +1,51 @@
+// const saveBtnElement = document.querySelector("button");
+
+// saveBtnElement.onclick = function () {
+//   console.log("Event Handler click 1");
+// };
+
+// saveBtnElement.onclick = function () {
+//   console.log("Event Handler click 2");
+// };
+
+// saveBtnElement.addEventListener("click", () => {
+//   console.log("Event listener click 1");
+// });
+// saveBtnElement.addEventListener("click", () => {
+//   console.log("Event listener click 2");
+// });
+// saveBtnElement.addEventListener("click", () => {
+//   console.log("Event listener click 3");
+// });
+// saveBtnElement.addEventListener("click", () => {
+//   console.log("Event listener click 4");
+// });
+
+// saveBtnElement.addEventListener("mouseenter", () => {
+//   console.log("mouse entered");
+// });
+// saveBtnElement.addEventListener("mouseleave", () => {
+//   console.log("mouse left");
+// });
+
+// saveBtnElement.addEventListener("mouseover", (event) => {
+//   console.log("mouse over", event);
+// });
+
+const inputElement = document.querySelector("input");
+
+// inputElement.addEventListener("focus", () => {
+//   console.log("focus olundu");
+// });
+// inputElement.addEventListener("blur", () => {
+//   console.log("focusdan cixdi");
+// });
+// inputElement.addEventListener("keydown", () => {
+// inputElement.addEventListener("keyup", () => {
+// inputElement.addEventListener("keypress", () => {
+//   console.log("basildi");
+// });
+
+// inputElement.addEventListener("keyup", (e) => {
+//   let value = e.target.value;
+// });
