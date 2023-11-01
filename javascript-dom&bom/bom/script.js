258d2d14a1a1639e239103511ddfeab6d1b91b42
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-11-01 12:21:45 +0400

    js bom added

---
+++ javascript-dom&bom/bom/script.js
@@ -0,0 +1,54 @@
+// document.querySelector("button").addEventListener("click", () => {
+//   alert("Hello");
+// });
+// document.querySelector("button").addEventListener("click", () => {
+//   const result = confirm("Are you sure to delete?");
+//   console.log(result);
+// });
+// document.querySelector("button").addEventListener("click", () => {
+//   const result = prompt("What is your name?");
+//   console.log(result);
+// });
+
+const btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   // window.location.reload();
+//   // console.log(window.history.back());
+//   // window.open(
+//   //   "https://dadacord.up.railway.app",
+//   //   "_blank",
+//   //   "location=yes,height=570,width=520,scrollbars=yes,status=yes"
+//   // );
+//   console.log("innerWidth", window.innerWidth);
+//   console.log("innerHeight", window.innerHeight);
+//   console.log("outerWidth", window.outerWidth);
+//   console.log("outerHeight", window.outerHeight);
+// });
+
+// window.addEventListener("resize", (e) => {
+//   console.log(window.innerWidth);
+//   console.log(window.innerHeight);
+// });
+
+// setTimeout(() => {
+//   console.log("time finished");
+// }, 1);
+
+// for (let i = 0; i < 100000; i++) {
+//   console.log(i);
+// }
+
+// let count = 1;
+
+// let id = setInterval(() => {
+//   console.log(count++);
+// }, 1000);
+
+// let timeOutId = setTimeout(() => {
+//   clearInterval(id);
+// }, 5000);
+
+// setTimeout(() => {
+//   clearTimeout(timeOutId);
+// }, 2000);
